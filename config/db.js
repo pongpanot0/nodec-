ded40e0517b040cbfe55d6abc8585476212dd69d
Pongpanot Samarkkarn <61924591+pongpanot0@users.noreply.github.com>
2022-07-22 17:49:13 +0700

    update

--- config/db.js
+++ config/db.js
@@ -1,34 +1,27 @@
-var mysql  = require('mssql');
 
-var mysql = require('mysql');
 
-var con = mysql.createConnection({
-  host: "119.59.97.193",
-  user: "root",
-  port:"33037",
-  password: "123456"
+var mysql = require('mysql2');
+
+
+let db = mysql.createConnection({
+    host: "119.59.97.193",
+    user: "root",
+    port:"33037",
+    password: "123456",
+    database: 'user'
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
-var connection = {
-    user:"sa",
-    password:"HIP@MASTER1234",
-    server:"119.59.97.193",
-    database:"ATTSENDLINE",
-    trustServerCertificate: true,
-    options: {
-        cryptoCredentialsDetails: {
-            minVersion: 'TLSv1'
-        }
+
+
+db.connect(function(err) {
+    if (err) {
+      return console.error('error: ' + err.message);
     }
-}
+  
+    console.log('Connected to the MySQL server.');
+  });
 
 
-
-const db = new mysql.ConnectionPool(connection);
 
 
 
