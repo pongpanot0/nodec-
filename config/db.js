648febab93cfccda159d7361f32e63c07d078742
Pongpanot Samarkkarn <61924591+pongpanot0@users.noreply.github.com>
2022-07-22 17:37:42 +0700

    update

--- config/db.js
+++ config/db.js
@@ -1,5 +1,18 @@
 var mysql  = require('mssql');
 
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "119.59.97.193",
+  user: "root",
+  port:"33037",
+  password: "123456"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 var connection = {
     user:"sa",
     password:"HIP@MASTER1234",
