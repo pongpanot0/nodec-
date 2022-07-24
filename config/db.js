af9b4f2ee6e17b40f8a31f2132369de0029eee70
Pongpanot Samarkkarn <61924591+pongpanot0@users.noreply.github.com>
2022-07-24 16:03:53 +0700

    update

--- config/db.js
+++ config/db.js
@@ -1,28 +1,55 @@
+var mysql  = require('mysql2');
+var connection = ({
+  host:'119.59.97.193',
+  user:'root',
+  password:'123456',
+  database: 'user',
+  port:'33037',
+})
 
+let db=null;
 
-var mysql = require('mysql2');
+function handleDisconnect() {
+  db = mysql.createPool(connection); // Recreate the connection, since
 
+  db.getConnection(function(err) { // The server is either down
+      if (err) { // or restarting (takes a while sometimes).
+          console.log('error when connecting to db:', err);
+          setTimeout(handleDisconnect, 1000); // We introduce a delay before attempting to reconnect,
+      } // to avoid a hot loop, and to allow our node script to
+  }); // process asynchronous requests in the meantime.
+  // If you're also serving http, display a 503 error.
+  db.on('error', function(err) {
+      console.log('db error', err);
+      if (err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+          handleDisconnect(); // lost due to either server restart, or a
 
-let db = mysql.createConnection({
-    host: "119.59.97.193",
-    user: "root",
-    port:"33037",
-    password: "123456",
-    database: 'user'
-});
+      } else if(err.code === "PROTOCOL_ENQUEUE_AFTER_QUIT"){
 
+      console.log("/!\\ Cannot establish a connection with the database. /!\\ ("+err.code+")");
+      handleDisconnect(); // lost due to either server restart, or a
 
+      } else if(err.code === "PROTOCOL_ENQUEUE_AFTER_FATAL_ERROR"){
 
-db.connect(function(err) {
-    if (err) {
-      return console.error('error: ' + err.message);
-    }
-  
-    console.log('Connected to the MySQL server.');
+        console.log("/!\\ Cannot establish a connection with the database. /!\\ ("+err.code+")");
+        handleDisconnect(); // lost due to either server restart, or a
+
+      } else { // connnection idle timeout (the wait_timeout
+          throw err; // server variable configures this)
+      }
   });
+}
 
-
-
+setInterval(function () {
+  db.query('SELECT 1');
+}, 120000);
+handleDisconnect(); 
 
 
 module.exports = db
+
+
+
+
+
+
