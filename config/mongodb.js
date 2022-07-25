7b348c786a3808d1e02bd224702bceecf370049f
Pongpanot Samarkkarn <61924591+pongpanot0@users.noreply.github.com>
2022-07-25 07:58:02 +0700

    kkk

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,7 +1,7 @@
 
 
 const { MongoClient } = require("mongodb");
-const uri = "mongodb://localhost:27018/";
+const uri = "mongodb://localhost:27017/";
 const conn = new MongoClient(uri);
 conn.connect();
 console.log('Start')
